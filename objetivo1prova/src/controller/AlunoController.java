c3fe7f0a02b0859176c40d2c51ba2aa4e1ce1653
pedromoreira49 <jpedrostonemoreira@gmail.com>
2022-11-03 15:24:34 -0300

    Created and printed Aluno instances in Controller Class

--- objetivo1prova/src/controller/AlunoController.java
+++ objetivo1prova/src/controller/AlunoController.java
@@ -1,4 +1,30 @@
 package controller;
 
+import model.AlunoModel;
+
 public class AlunoController {
+  public static void main(String[] args){
+      //Build instances
+
+      //Instancia padrão
+      AlunoModel alunoA = new AlunoModel();
+      AlunoModel alunoB = new AlunoModel();
+
+      //Instancia parametrizado
+      AlunoModel alunoC = new AlunoModel(3, "12312312366", "Pedro", "Moreira", "pedro@moreira.com");
+      AlunoModel alunoD = new AlunoModel(4, "89089089044", "Ricardo", "Moreira", "ricardo@moreira.com");
+
+      //Instancia parametrizado com 2 parametros
+      AlunoModel alunoE = new AlunoModel(5, "Rodrigo", "Moreira");
+      AlunoModel alunoF = new AlunoModel(6, "Thalia", "Santos");
+
+      //Printing instances created
+      System.out.println("Printing instances:");
+      System.out.println(alunoA);
+      System.out.println(alunoB);
+      System.out.println(alunoC);
+      System.out.println(alunoD);
+      System.out.println(alunoE);
+      System.out.println(alunoF);
+  }
 }
