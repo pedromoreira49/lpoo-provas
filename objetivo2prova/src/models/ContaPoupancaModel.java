44936ec9998e620f0d6a5fbf53d130df16658bdf
pedromoreira49 <jpedrostonemoreira@gmail.com>
2022-11-27 21:40:52 -0300

    adicionado conta poupanca model

---
+++ objetivo2prova/src/models/ContaPoupancaModel.java
@@ -0,0 +1,10 @@
+package models;
+
+public class ContaPoupancaModel extends ContaModel{
+    @Override
+    public String toString() {
+        return "ContaPoupancaModel{" +
+                "saldo=" + saldo +
+                '}';
+    }
+}
