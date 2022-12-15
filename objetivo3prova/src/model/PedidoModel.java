8a60775aa12be2d28452756d877b2621d3e0e5ae
pedromoreira49 <jpedrostonemoreira@gmail.com>
2022-12-15 11:16:09 -0300

    adicionado classe Pedido do package model

---
+++ objetivo3prova/src/model/PedidoModel.java
@@ -0,0 +1,103 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class PedidoModel {
+    private int numero;
+    private Date data;
+    private double valor;
+    private Tipo tipo;
+    private VendedorModel vendedor;
+    private List<ItemModel> itens = new ArrayList<>();
+
+    public PedidoModel(){
+        super();
+    }
+
+    public PedidoModel(int numero, Date data, Tipo tipo, VendedorModel vendedor, List<ItemModel> itens){
+        super();
+        this.numero = numero;
+        this.data = data;
+        this.tipo = tipo;
+        this.vendedor = vendedor;
+        this.itens = itens;
+    }
+
+    public PedidoModel(int numero, Date data, Tipo tipo, VendedorModel vendedor, ItemModel item){
+        super();
+        this.numero = numero;
+        this.data = data;
+        this.tipo = tipo;
+        this.vendedor = vendedor;
+        this.itens.add(item);
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public Tipo getTipo() {
+        return tipo;
+    }
+
+    public VendedorModel getVendedor() {
+        return vendedor;
+    }
+
+    public List<ItemModel> getItens() {
+        return itens;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public void setValor() {
+        this.valor = this.itens.stream().mapToDouble(item -> item.getTotal_item()).sum();
+    }
+
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
+    }
+
+    public void setVendedor(VendedorModel vendedor) {
+        this.vendedor = vendedor;
+    }
+
+    public void setItem(ItemModel item){ this.itens.add(item); }
+    public void setItens(List<ItemModel> itens) {
+        this.itens = itens;
+    }
+
+    public void atualizaEstoque() {
+        itens.forEach(item -> {
+            item.getProduto().setQuantidade(item.getProduto().getQuantidade() -item.getQuantidade());
+        });
+    }
+
+    @Override
+    public String toString() {
+        return "PedidoModel{" +
+                "numero=" + numero +
+                ", data=" + data +
+                ", valor=" + valor +
+                ", tipo=" + tipo +
+                ", vendedor=" + vendedor +
+                ", itens=" + itens +
+                '}';
+    }
+}
