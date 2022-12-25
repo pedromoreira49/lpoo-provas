5e1dc888942907f85d4d72a5610b8a99c158a50d
pedromoreira49 <jpedrostonemoreira@gmail.com>
2022-12-25 16:53:50 -0300

    Modificado paciente DAO

--- objetivo5prova/src/main/java/DAO/PacienteDAO.java
+++ objetivo5prova/src/main/java/DAO/PacienteDAO.java
@@ -73,7 +73,27 @@ public class PacienteDAO extends BaseDAO{
         }
     }
 
-    public static boolean selectForLogin(String email, String senha){
+    public static List<PacienteModel> selectPacienteNotPassword(){
+        final String sql = "SELECT nome, email, cpf FROM paciente ORDER BY nome";
+        try
+                (
+                        Connection conn = getConnection();
+                        PreparedStatement pstmt = conn.prepareStatement(sql);
+                        ResultSet rs = pstmt.executeQuery();
+                )
+        {
+            List<PacienteModel> pacientes = new ArrayList<>();
+            while(rs.next()) {
+                pacientes.add(resultsetToPacienteNotPassword(rs));
+            }
+            return pacientes;
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static PacienteModel selectForLogin(String email, String senha){
         final String sql = "SELECT * FROM paciente WHERE email = ? AND senha = ? AND status = 1";
         try
             (
@@ -84,16 +104,15 @@ public class PacienteDAO extends BaseDAO{
             pstmt.setString(1, email);
             pstmt.setString(2, senha);
             ResultSet rs = pstmt.executeQuery();
+            PacienteModel paciente = null;
             if(rs.next()){
-                rs.close();
-                return true;
-            }else{
-                rs.close();
-                return false;
+                paciente = resultsetToPaciente(rs);
             }
+            rs.close();
+            return paciente;
         }catch(SQLException e){
             e.printStackTrace();
-            return false;
+            return null;
         }
     }
 
@@ -169,39 +188,51 @@ public class PacienteDAO extends BaseDAO{
         return paciente;
     }
 
+    private static PacienteModel resultsetToPacienteNotPassword(ResultSet rs) throws SQLException {
+        PacienteModel paciente = new PacienteModel();
+        paciente.setNome(rs.getString("nome"));
+        paciente.setEmail(rs.getString("email"));
+        paciente.setCpf(rs.getString("cpf"));
+        return paciente;
+    }
+
     public static void main(String[] args){
         //Listando todos pacientes
-        System.out.println("Printando pacientes");
-        System.out.println(PacienteDAO.selectPacientes());
+        //System.out.println("Printando pacientes");
+        //System.out.println(PacienteDAO.selectPacientes());
 
         //Criando um paciente
-        System.out.println("Criando um paciente");
-        PacienteModel paciente = new PacienteModel(2L, "Thalia", "thalia@gmail.com", "123123", "12312312366", false);
-        System.out.println(PacienteDAO.insertPaciente(paciente));
-        System.out.println("Paciente adicionado com sucesso:" + paciente);
+//        System.out.println("Criando um paciente");
+//        PacienteModel paciente = new PacienteModel(2L, "Thalia", "thalia@gmail.com", "123123", "12312312366", false);
+//        System.out.println(PacienteDAO.insertPaciente(paciente));
+//        System.out.println("Paciente adicionado com sucesso:" + paciente);
+//
+//        //Apresentando novo SQL
+//        System.out.println(PacienteDAO.selectPacientes());
+//
+//        //Printando paciente pelo ID
+//        System.out.println("Printando paciente pelo ID");
+//        System.out.println(PacienteDAO.selectPacienteById(2L));
+//
+//        //Printando paciente pelo nome
+//        System.out.println("Printando paciente pelo nome");
+//        System.out.println(PacienteDAO.selectPacienteByName("t"));
+//
+//        //Alterando um paciente
+//        paciente = selectPacienteById(2L);
+//        paciente.setNome("Thalia");
+//        paciente.setEmail("santos@gmail.com");
+//        paciente.setSenha("345345");
+//        paciente.setCpf("55555555555");
+//        paciente.setStatus(true);
+//        System.out.println(PacienteDAO.updatePaciente(paciente));
+//
+//        //Realizando um soft delete em paciente
+//        System.out.println("Realizando um soft DELETE em paciente");
+//        System.out.println(PacienteDAO.softDeletePaciente(2, false));
 
-        //Apresentando novo SQL
-        System.out.println(PacienteDAO.selectPacientes());
-
-        //Printando paciente pelo ID
-        System.out.println("Printando paciente pelo ID");
-        System.out.println(PacienteDAO.selectPacienteById(2L));
-
-        //Printando paciente pelo nome
-        System.out.println("Printando paciente pelo nome");
-        System.out.println(PacienteDAO.selectPacienteByName("t"));
-
-        //Alterando um paciente
-        paciente = selectPacienteById(2L);
-        paciente.setNome("Thalia");
-        paciente.setEmail("santos@gmail.com");
-        paciente.setSenha("345345");
-        paciente.setCpf("55555555555");
-        paciente.setStatus(true);
-        System.out.println(PacienteDAO.updatePaciente(paciente));
-
-        //Realizando um soft delete em paciente
-        System.out.println("Realizando um soft DELETE em paciente");
-        System.out.println(PacienteDAO.softDeletePaciente(2, false));
+//        //Fazendo login
+//        System.out.println("Fazendo login");
+//        System.out.println(PacienteDAO.selectForLogin("santos@gmail.com", "345345"));
     }
 }
