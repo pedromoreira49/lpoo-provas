1a9160d71adc46400c52fb81d303c9f60af0d12b
pedromoreira49 <jpedrostonemoreira@gmail.com>
2022-12-25 16:56:01 -0300

    Adicionado classe Paciente do package model

---
+++ objetivo5prova/src/main/java/model/PacienteModel.java
@@ -0,0 +1,31 @@
+package model;
+
+public class PacienteModel extends UsuarioModel{
+    public PacienteModel(){
+        super();
+    }
+
+    public PacienteModel(Long id, String nome, String email, String senha, String cpf, boolean status){
+        super(id, nome, email, senha, cpf, status);
+    }
+
+    public PacienteModel(String nome, String email, String senha, String cpf, boolean status){
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.cpf = cpf;
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "PacienteModel{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", senha='" + senha + '\'' +
+                ", email='" + email + '\'' +
+                ", cpf='" + cpf + '\'' +
+                ", status=" + status +
+                '}';
+    }
+}
